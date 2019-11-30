544d5b28b67850a73ad73c780b0a5f817988d702
shsangha <sssangha@ucalgary.ca>
2019-11-29 21:57:30 -0700

    moved my created types and declarations into seperate files for readability

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+declare module "gatsby-plugin-transition-link"
+
+declare module "*.scss"
+
+declare module "*DrawSVGPlugin"
+
+declare module "body-scroll-lock"
+
+declare module "*EasePack.min.js"
---
+++ types/index.ts
@@ -0,0 +1,10 @@
+export interface ReachRouterLocation extends Location {
+  state: {
+    key: string
+  }
+}
+
+export interface MenuState {
+  menuOpen: boolean
+  menuVisible: boolean
+}
