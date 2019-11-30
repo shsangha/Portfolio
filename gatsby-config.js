297f534a47217f94b11fe77a4768633a31600ab4
shsangha <sssangha@ucalgary.ca>
2019-11-29 21:51:01 -0700

    removed plugin for links bc going to be using react-transition-group to be able to handle popstate

---
+++ gatsby-config.js
@@ -0,0 +1,93 @@
+const proxy = require("http-proxy-middleware")
+
+module.exports = {
+  pathPrefix: "/",
+  siteMetadata: {
+    siteUrl: "https://ssangha.io",
+    pathPrefix: "/",
+    title: "Shawn Sangha",
+    titleAlt: "Shawn Sangha",
+    description:
+      "Shawn Sangha. Front-end developer currently based in Calgary.",
+    banner: "/img/card.jpg",
+    headline: "Dev portfolio of Shawn Sangha",
+    siteLanguage: "en",
+    author: "Shawn Sangha",
+    ogLanguage: "en_US",
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    "gatsby-plugin-typescript",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/static/img`,
+        name: "uploads",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/content`,
+        name: "cmsContent",
+      },
+    },
+
+    `gatsby-plugin-sharp`,
+    {
+      resolve: "gatsby-transformer-sharp",
+      options: {
+        stripMetadata: true,
+        defaultQuality: 75,
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-relative-images",
+            options: {
+              name: "uploads",
+            },
+          },
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 2048,
+            },
+          },
+          {
+            resolve: "gatsby-remark-copy-linked-files",
+            options: {
+              destinationDir: "static",
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-layout`,
+      options: {
+        component: require.resolve(`./src/layouts/index.tsx`),
+      },
+    },
+    "gatsby-plugin-sitemap",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Shawn Sangha`,
+        short_name: `Shawn Sangha`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `static/img/card.jpg`,
+      },
+    },
+    "gatsby-plugin-netlify-cms",
+    `gatsby-plugin-offline`,
+    // "gatsby-plugin-netlify"
+  ],
+}
