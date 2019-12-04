0d5faff85b16708a899d6816a1d76df71e4f8763
shsangha <sssangha@ucalgary.ca>
2019-12-03 21:35:30 -0700

    wrappeed in hoc for intersection observer animation

---
+++ src/pages/projectsPage/index.tsx
@@ -0,0 +1,4 @@
+import IntersectionObserverTriggerHOC from "../../components/IntersectionObserverTriggerHOC"
+import Projects from "./projects"
+
+export default IntersectionObserverTriggerHOC(Projects)
