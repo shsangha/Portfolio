e1038eb631a7d78e18ffd2ca3a23b34ade666e17
shsangha <sssangha@ucalgary.ca>
2020-01-19 17:52:12 -0700

    added custom type declaration for fluid images

--- types/index.ts
+++ types/index.ts
@@ -8,3 +8,15 @@ export interface MenuState {
   menuOpen: boolean
   menuVisible: boolean
 }
+
+export interface FluidImage {
+  childImageSharp: {
+    fluid: {
+      aspectRatio: number
+      base64: string
+      sizes: string
+      src: string
+      srcSet: string
+    }
+  }
+}
