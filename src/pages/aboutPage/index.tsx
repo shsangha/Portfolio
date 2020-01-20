ac3c97555faaff0438a2b4f249997ad90a881fa3
shsangha <sssangha@ucalgary.ca>
2020-01-19 17:57:43 -0700

    unanimated first go an about page, wont change layout just need to connect to cms and add svg animations

--- src/pages/aboutPage/index.tsx
+++ src/pages/aboutPage/index.tsx
@@ -1,63 +1,79 @@
 import React, { useState } from "react"
+import SubHead from "../../components/subHead"
 import "./index.scss"
 
-export default (props: any) => {
-  const [selected, setSelected] = useState<"dev" | "person">("dev")
-
-  return (
-    <div className="about_container">
-      <h2 className="about_header">Shawn Sangha</h2>
-      <div className="about_img_wrapper" {...props.contrastCursor()}>
+export default () => (
+  <>
+    <div className="about_page">
+      <div className="about_content">
+        <SubHead mode="dark">About Me</SubHead>
         <img
-          className={`about_img about_img_person  ${
-            selected === "person" ? "about_img_selected" : ""
-          }`}
-          src="https://images.unsplash.com/photo-1542914119-031b7a0dbb8a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          className="about_img"
+          src="https://images.unsplash.com/photo-1533075377664-f5c0cbc5a91c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
         />
-        <img
-          className={`about_img about_img_dev  ${
-            selected === "dev" ? "about_img_selected" : ""
-          }`}
-          src="https://images.unsplash.com/photo-1540569014015-19a7be504e3a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-        />
-      </div>
-      <div className="about_nav">
-        <button
-          onClick={() => {
-            setSelected("dev")
-          }}
-          className={`about_nav_btn ${
-            selected === "dev" ? "about_nav_selected" : ""
-          }`}
-          {...props.focusLink()}
-        >
-          The Dev
-        </button>
-        <button
-          onClick={() => setSelected("person")}
-          className={`about_nav_btn ${
-            selected === "person" ? "about_nav_selected" : ""
-          }`}
-          {...props.focusLink()}
-        >
-          The Person
-        </button>
-      </div>
-      <div className="about_desc">
-        {selected === "dev" ? (
-          <p className={`about_desc_text`}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime
-            impedit nihil veniam cum architecto perspiciatis labore assumenda
-            excepturi delectus, optio voluptatibus ut, pariatur magnam hic vero
-            veritatis iusto ullam. Quasi!
-          </p>
-        ) : (
-          <p className={`about_desc_text`}>
-            suscipit corporis perferendis, sint recusandae aspernatur impedit,
-            ut dolorum quidem soluta.
-          </p>
-        )}
       </div>
     </div>
-  )
-}
+    <h2 className="about_page_summary">
+      <p className="about_page_summary_text">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur
+        atque nostrum doloribus et dolore mollitia voluptas incidunt cupiditate
+        soluta alias magni, assumenda fugiat veritatis itaque repellat in
+        recusandae ipsam. Esse.
+      </p>
+    </h2>
+    <h2 className="about_page_mission">
+      <p className="about_page_mission_text">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa a
+        assumenda aspernatur similique voluptatibus earum mollitia maxime
+      </p>
+    </h2>
+    <div className="about_page_row">
+      <div className="about_page_row_content">
+        <h3 className="about_page_row_header">How I work</h3>
+        <p className="about_page_row_text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est,
+          repellendus laudantium laboriosam quasi perferendis molestiae.
+          Voluptatibus doloremque, consequatur recusandae necessitatibus
+          corrupti eum ut molestiae blanditiis corporis dicta adipisci saepe
+          numquam.
+        </p>
+      </div>
+    </div>
+    <div className="about_page_row">
+      <div className="about_page_row_content">
+        <h3 className="about_page_row_header">How I work</h3>
+        <p className="about_page_row_text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est,
+          repellendus laudantium laboriosam quasi perferendis molestiae.
+          Voluptatibus doloremque, consequatur recusandae necessitatibus
+          corrupti eum ut molestiae blanditiis corporis dicta adipisci saepe
+          numquam.
+        </p>
+      </div>
+    </div>
+    <div className="about_page_row">
+      <div className="about_page_row_content">
+        <h3 className="about_page_row_header">How I work</h3>
+        <p className="about_page_row_text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est,
+          repellendus laudantium laboriosam quasi perferendis molestiae.
+          Voluptatibus doloremque, consequatur recusandae necessitatibus
+          corrupti eum ut molestiae blanditiis corporis dicta adipisci saepe
+          numquam.
+        </p>
+      </div>
+    </div>
+    <div className="about_page_row">
+      <div className="about_page_row_content">
+        <h3 className="about_page_row_header">How I work</h3>
+        <p className="about_page_row_text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est,
+          repellendus laudantium laboriosam quasi perferendis molestiae.
+          Voluptatibus doloremque, consequatur recusandae necessitatibus
+          corrupti eum ut molestiae blanditiis corporis dicta adipisci saepe
+          numquam.
+        </p>
+      </div>
+    </div>
+  </>
+)
