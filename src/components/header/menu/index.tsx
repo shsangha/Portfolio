ea4eee66030b4d7c2239c7dd852c7695266114b9
shsangha <sssangha@ucalgary.ca>
2020-02-09 22:55:43 -0700

    colocated the menu animation code by handling all of it in once place within  a useeffect based on status of the main transiion in the layout

--- src/components/header/menu/index.tsx
+++ src/components/header/menu/index.tsx
@@ -1,12 +1,5 @@
-import React, {
-  useEffect,
-  useState,
-  useRef,
-  Dispatch,
-  SetStateAction,
-} from "react"
+import React, { Component, Dispatch, SetStateAction } from "react"
 import gsap from "gsap"
-import Loadable from "react-loadable"
 import { Link } from "gatsby"
 
 import { MenuState } from "../../../../types/index"
@@ -26,185 +19,252 @@ import contact1 from "../../../../static/img/menuContact1.jpg"
 import contact2 from "../../../../static/img/menuContact2.jpg"
 import contact3 from "../../../../static/img/menuContact3.jpg"
 
+interface StatefulLocation extends Location {
+  state: {
+    animation: string
+    target: string
+  }
+}
+
 interface Props {
-  location: Location
+  location: StatefulLocation
   menuStatus: MenuState
   setMenuStatus: Dispatch<SetStateAction<MenuState>>
   menuType: string
 }
 
-let tl: any
+export default class Menu extends Component<Props> {
+  state = {
+    hovered: "",
+    transitioning: "",
+  }
 
-const Background = Loadable({
-  loader: () => import("./Background"),
-  loading: () => null,
-})
+  menuToggleTL: GSAPTimeline | null = null
+  navigationChangeTL: GSAPTimeline | null = null
 
-export default ({ setMenuStatus, menuStatus, location, menuType }: Props) => {
-  const menuRef = useRef<HTMLDivElement>(null)
+  public componentDidMount(): void {
+    const { setMenuStatus } = this.props
 
-  const [hovered, setHovered] = useState("")
-
-  useEffect(() => {
-    tl = gsap
+    this.menuToggleTL = gsap
       .timeline({
-        paused: true,
         onReverseComplete: () => {
           setMenuStatus({
-            menuOpen: false,
-            menuVisible: false,
+            //     menuOpen: false,
+            //      menuVisible: false,
           })
         },
       })
       .to(".menu", 0.8, { y: 0, ease: "power2.in" })
       .to(".menu", 1, { opacity: 1, ease: "power2" }, "-=0.8")
-      .to(".menu_btn", 0.5, { rotation: "45deg" }, "-=0.5")
+      .fromTo(".menu_btn", 0.5, { rotation: 0 }, { rotation: "45deg" }, "-=0.5")
       .staggerTo(
         ".menu_link_svg",
-        0.3,
+        0.2,
         {
-          background: "rgba(0,0,0, 0)",
+          opacity: 1,
           ease: "power1",
         },
         0.2,
-        "-=0.2"
+        "-=0.3"
       )
-  }, [])
+  }
 
-  useEffect(() => {
-    if (menuStatus.menuVisible) {
-      tl.play()
-    } else {
-      tl.reverse()
+  public componentDidUpdate(prevProps: Props): void {
+    const {
+      menuStatus: { menuVisible },
+      setMenuStatus,
+      location,
+    } = this.props
+
+    const { menuToggleTL, navigationChangeTL } = this
+
+    if (prevProps.menuStatus.menuVisible !== menuVisible && !menuVisible) {
+      menuToggleTL && menuToggleTL.reverse()
     }
-  }, [menuStatus.menuVisible])
 
-  return (
-    <div className="menu">
-      <div className={`menu_wrapper ${menuType}`}>
-        <div className="menu_content animation_controller_slideup">
-          <Background />
-          {["about", "skills", "work", "contact"].map(item => (
-            <Link
-              key={item}
-              to={`/${item}`}
-              state={{ animation: "slide-up" }}
-              className={`menu_link ${item} ${menuType} ${
-                hovered && item !== hovered ? "not_hovered" : ""
-              }`}
-            >
-              <svg
-                overflow="visible"
-                width="100%"
-                height="100%"
-                className="menu_link_svg"
-                onMouseEnter={() => {
-                  setHovered(item)
-                }}
-                onMouseLeave={() => {
-                  setHovered("")
-                }}
-                viewBox="0 0 400 120"
-                preserveAspectRatio="none"
+    if (
+      location.pathname !== prevProps.location.pathname &&
+      location.state &&
+      location.state.target
+    ) {
+      if (navigationChangeTL && navigationChangeTL.progress() < 1) {
+        navigationChangeTL.progress(100) // play around with this
+        navigationChangeTL.clear()
+      }
+
+      this.navigationChangeTL = gsap
+        .timeline()
+        .add(() => {
+          this.setState({
+            transitioning: location.state.target,
+          })
+        })
+        .set([".menu_link_svg", ".menu_btn"], { pointerEvents: "none" })
+        .set(".menu_cover", { zIndex: 2 })
+        .set(".menu", { zIndex: 1 })
+        .addLabel("start")
+        .fromTo(
+          ".menu_btn",
+          1,
+          {
+            rotation: "45deg",
+          },
+          {
+            pointerEvents: "all",
+            rotation: "0",
+            ease: "power2.in",
+          },
+          "start"
+        )
+        .add(() => {
+          setMenuStatus({
+            menuOpen: false,
+            menuVisible: false,
+          })
+        })
+    }
+  }
+
+  public setHovered = (hovered: string): void => {
+    this.setState({
+      hovered,
+    })
+  }
+  public render(): JSX.Element {
+    const { hovered, transitioning } = this.state
+    const { menuType } = this.props
+    const { setHovered } = this
+
+    return (
+      <div className="menu">
+        <div className={`menu_wrapper ${menuType}`}>
+          <div className="menu_content animation_controller_slideup">
+            {["about", "skills", "work", "contact"].map(item => (
+              <Link
+                key={item}
+                to={`/${item}`}
+                state={{ animation: "menu_animation", target: `${item}` || "" }}
+                className={`menu_link ${item} ${menuType} ${
+                  (hovered && item !== hovered) ||
+                  (transitioning && transitioning && item !== transitioning)
+                    ? "not_hovered"
+                    : ""
+                }`}
               >
-                <defs>
-                  <mask
-                    id={`mask_${item}`}
-                    x="0"
-                    y="0"
-                    width="100%"
-                    height="100%"
-                  >
-                    <rect
+                <svg
+                  overflow="visible"
+                  width="100%"
+                  height="100%"
+                  className="menu_link_svg"
+                  onMouseEnter={() => {
+                    setHovered(item)
+                  }}
+                  onMouseLeave={() => {
+                    setHovered("")
+                  }}
+                  viewBox="0 0 400 120"
+                  preserveAspectRatio="none"
+                >
+                  <defs>
+                    <mask
+                      id={`mask_${item}`}
                       x="0"
                       y="0"
                       width="100%"
                       height="100%"
-                      fill="white"
-                      shapeRendering="crispEdges"
-                    />
-                    <text
-                      fontSizeAdjust="0.4"
-                      textAnchor="middle"
-                      alignmentBaseline="middle"
-                      dominantBaseline="middle"
-                      lengthAdjust="spacingAndGlyphs"
-                      x="50%"
-                      y="50%"
-                      shapeRendering="crispEdges"
                     >
-                      {item}
-                    </text>
-                  </mask>
-                </defs>
-                <rect
-                  mask={`url(#mask_${item})`}
-                  x="0"
-                  y="0"
-                  width="100%"
-                  height="100%"
-                  shapeRendering="crispEdges"
-                  stroke-width="10px"
-                  fill={menuType === "light" ? "white" : "black"}
-                />
-              </svg>
-            </Link>
-          ))}
+                      <rect
+                        x="0"
+                        y="0"
+                        width="100%"
+                        height="100%"
+                        fill="white"
+                        shapeRendering="crispEdges"
+                      />
+                      <text
+                        fontSizeAdjust="0.4"
+                        textAnchor="middle"
+                        alignmentBaseline="middle"
+                        dominantBaseline="middle"
+                        lengthAdjust="spacingAndGlyphs"
+                        x="50%"
+                        y="50%"
+                        shapeRendering="crispEdges"
+                      >
+                        {item}
+                      </text>
+                    </mask>
+                  </defs>
+                  <rect
+                    mask={`url(#mask_${item})`}
+                    x="0"
+                    y="0"
+                    width="100%"
+                    height="100%"
+                    shapeRendering="crispEdges"
+                    strokeWidth="10px"
+                    fill={menuType === "light" ? "#d4d6d8" : "#0c0c0c"}
+                  />
+                </svg>
+              </Link>
+            ))}
 
-          {hovered === "about" && (
-            <>
-              <div className="menu_img_about1 menu_img">
-                <img className="imginner" src={about1} />
-              </div>
-              <div className="menu_img_about2 menu_img">
-                <img className="imginner" src={about2} />
-              </div>
-              <div className="menu_img_about3 menu_img">
-                <img className="imginner" src={about3} />
-              </div>
-            </>
-          )}
-          {hovered === "skills" && (
-            <>
-              <div className="menu_img_skills1 menu_img">
-                <img className="imginner" src={skills1} />
-              </div>
-              <div className="menu_img_skills2 menu_img">
-                <img className="imginner" src={skills2} />
-              </div>
-              <div className="menu_img_skills3 menu_img">
-                <img className="imginner" src={skills3} />
-              </div>
-            </>
-          )}
-          {hovered === "work" && (
-            <>
-              <div className="menu_img_work1 menu_img">
-                <img className="imginner" src={work} />
-              </div>
-              <div className="menu_img_work2 menu_img">
-                <img className="imginner" src={work} />
-              </div>
-              <div className="menu_img_work3 menu_img">
-                <img className="imginner" src={work} />
-              </div>
-            </>
-          )}
-          {hovered === "contact" && (
-            <>
-              <div className="menu_img_contact1 menu_img">
-                <img className="imginner" src={contact1} />
-              </div>
-              <div className="menu_img_contact2 menu_img">
-                <img className="imginner" src={contact2} />
-              </div>
-              <div className="menu_img_contact3 menu_img">
-                <img className="imginner" src={contact3} />
-              </div>
-            </>
-          )}
+            {(hovered === "about" || transitioning === "about") && (
+              <>
+                <div className="menu_img_about one menu_img">
+                  <img className="imginner" src={about1} />
+                </div>
+                <div className="menu_img_about two menu_img">
+                  <img className="imginner" src={about2} />
+                </div>
+                <div className="menu_img_about three menu_img">
+                  <img className="imginner" src={about3} />
+                </div>
+              </>
+            )}
+            {(hovered === "skills" || transitioning === "skills") && (
+              <>
+                <div className="menu_img_skills one menu_img ">
+                  <img className="imginner" src={skills1} />
+                </div>
+                <div className="menu_img_skills two menu_img">
+                  <img className="imginner" src={skills2} />
+                </div>
+                <div className="menu_img_skills three menu_img">
+                  <img className="imginner" src={skills3} />
+                </div>
+              </>
+            )}
+            {(hovered === "work" || transitioning === "work") && (
+              <>
+                <div className="menu_img_work one menu_img">
+                  <img className="imginner" src={work} />
+                </div>
+                <div className="menu_img_work two menu_img">
+                  <img className="imginner" src={work} />
+                </div>
+                <div className="menu_img_work three menu_img">
+                  <img className="imginner" src={work} />
+                </div>
+              </>
+            )}
+            {(hovered === "contact" || hovered === "contact") && (
+              <>
+                <div className="menu_img_contact one menu_img">
+                  <img className="imginner" src={contact1} />
+                </div>
+                <div className="menu_img_contact two menu_img">
+                  <img className="imginner" src={contact2} />
+                </div>
+                <div className="menu_img_contact three menu_img">
+                  <img className="imginner" src={contact3} />
+                </div>
+              </>
+            )}
+          </div>
         </div>
+        <div className="menu_cover" />
       </div>
-    </div>
-  )
+    )
+  }
 }
